a7359ac61c9315236e0fea7e273cdea68b9584fa
hiralirs <hiralirs@gmail.com>
2026-08-18 05:37:01 +0530

    Add basic variable lesson

---
+++ 01_Basics/01_variables.py
@@ -0,0 +1,11 @@
+#01_variables.py
+
+student_name = "Hirali"       #String (Text)
+current_semester = 3          #Integer (Whole number)
+current_gpa = 8.73            #Float (Decimal)
+is_enrolled = True            #Boolean (True/False)
+
+print("Name:", student_name)
+print("GPA:", current_gpa)
+print("Data type of GPA:", type(current_gpa))
+
